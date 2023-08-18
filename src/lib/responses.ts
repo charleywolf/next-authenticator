554765551254d3008ed35082990ffb757673b982
neoncodes <41237443+charleywolf@users.noreply.github.com>
2023-08-18 17:29:19 -0400

    Fixed build command & added factory responses

--- src/lib/responses.ts
+++ src/lib/responses.ts
@@ -1,1 +1,19 @@
-// return factories for each response type
+import { NextResponse } from "next/server";
+
+const Success = (): NextResponse => {
+  return NextResponse.json({ status: 200 });
+};
+
+const InternalServerError = (): NextResponse => {
+  return NextResponse.json({ status: 500 });
+};
+
+const Unauthorized = (): NextResponse => {
+  return NextResponse.json({ status: 401 }); // client provides no credentials or invalid credentials
+};
+
+const Forbidden = (): NextResponse => {
+  return NextResponse.json({ status: 403 }); // client has valid credentials but not enough privileges to perform an action on a resource
+};
+
+export { Forbidden, Unauthorized, InternalServerError, Success };
