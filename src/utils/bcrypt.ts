72bc0d3be73425950590bce46c333a104dcc2185
neoncodes <41237443+charleywolf@users.noreply.github.com>
2023-08-19 16:54:28 -0400

    converted from bcrypt to built-in crypto module

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,22 +1,31 @@
-import bcrypt from "bcrypt";
 import { getError, throwError } from "./misc";
-const saltRounds: number = 10;
+import crypto from "crypto";
+
+const saltLength: number = 16; // Bytes
 
 export async function hashPassword(password: string): Promise<string> {
   try {
-    return await bcrypt.hash(password, saltRounds);
-  } catch (error: unknown) {
+    const salt: string = crypto.randomBytes(saltLength).toString("hex");
+    const hash: string = crypto
+      .pbkdf2Sync(password, salt, 100000, 64, "sha512")
+      .toString("hex");
+    return `${salt}:${hash}`;
+  } catch (error) {
     throw Error(getError("hashPassword", error));
   }
 }
 
 export async function comparePasswords(
   password: string,
-  hash: string,
+  storedPassword: string,
 ): Promise<boolean> {
   try {
-    return await bcrypt.compare(password, hash);
-  } catch (error: unknown) {
+    const [salt, hash] = storedPassword.split(":");
+    const newHash: string = crypto
+      .pbkdf2Sync(password, salt, 100000, 64, "sha512")
+      .toString("hex");
+    return newHash === hash;
+  } catch (error) {
     throwError("comparePasswords", error);
     return false;
   }
