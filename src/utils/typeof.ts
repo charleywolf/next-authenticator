1a47e416d218637d5220b4fde709be3e7bd15db4
neoncodes <41237443+charleywolf@users.noreply.github.com>
2023-08-20 21:21:39 -0400

    Found complex boolean return JS-W1041

--- src/utils/typeof.ts
+++ src/utils/typeof.ts
@@ -1,16 +1,12 @@
 import { SessionSecret } from "../lib/interface";
 
 export function isSessionSecret(obj: unknown): obj is SessionSecret {
-  if (
+  return (
     typeof obj === "object" &&
     obj !== null &&
     "username" in obj &&
     "expiration" in obj &&
     typeof obj.username !== "string" &&
     typeof obj.expiration !== "number"
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+  );
 }
